8a4a7b7dc09748390a6d4604dbdcc1aa5cfb7e1b
MinsukKim1806 <minsuk.kim.1806@gmail.com>
2023-07-30 21:20:50 +0200

    Basic menu icons

--- v1/lib/widgets/containers/menu.dart
+++ v1/lib/widgets/containers/menu.dart
@@ -6,66 +6,138 @@ Container manuContainer(){
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Container(color: Colors.white,
-         child: Row(
+        Flexible (
+          flex: 10,
+          child: Container(color: Colors.white,
+            child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-              Container(color: Colors.pink,
+              Container(
+                padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                height: 40.0,
+                decoration: BoxDecoration(
+                  color: Colors.lightBlue,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 4,
+                      offset: Offset(4, 8), // Shadow position
+                    ),
+                  ],
+                ),
                 child: const Center(
                   child: Text(
                     'Starter',
-                    style: TextStyle(fontSize: 35),
+                    style: TextStyle(fontSize: 25),
                   ),
                 ),
               ),
-              SizedBox(width: 10,),
-              Container(color: Colors.pink,
+              const SizedBox(width: 10,),
+              Container(
+                padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                height: 40.0,
+                decoration: BoxDecoration(
+                  color: Colors.lightBlue,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 4,
+                      offset: Offset(4, 8), // Shadow position
+                    ),
+                  ],
+                ),
                 child: const Center(
                   child: Text(
                     'Main',
-                    style: TextStyle(fontSize: 35),
+                    style: TextStyle(fontSize: 25),
                   ),
                 ),
               ),
-              SizedBox(width: 10,),
-              Container(color: Colors.pink,
+              const SizedBox(width: 10,),
+              Container(
+                padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                height: 40.0,
+                decoration: BoxDecoration(
+                  color: Colors.lightBlue,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 4,
+                      offset: Offset(4, 8), // Shadow position
+                    ),
+                  ],
+                ),
                 child: const Center(
                   child: Text(
                     'Desert',
-                    style: TextStyle(fontSize: 35),
+                    style: TextStyle(fontSize: 25),
                   ),
                 ),
               ),
-              SizedBox(width: 10,),
-              Container(color: Colors.pink,
+              const SizedBox(width: 10,),
+              Container(
+                padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                height: 40.0,
+                decoration: BoxDecoration(
+                  color: Colors.lightBlue,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 4,
+                      offset: Offset(4, 8), // Shadow position
+                    ),
+                  ],
+                ),
                 child: const Center(
                   child: Text(
                     'Drinks',
-                    style: TextStyle(fontSize: 35),
+                    style: TextStyle(fontSize: 25),
                   ),
                 ),
               ),
-              SizedBox(width: 10,),
-              Container(color: Colors.pink,
+              const SizedBox(width: 10,),
+              Container(
+                padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                height: 40.0,
+                decoration: BoxDecoration(
+                  color: Colors.lightBlue,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 4,
+                      offset: Offset(4, 8), // Shadow position
+                    ),
+                  ],
+                ),
                 child: const Center(
                   child: Text(
                     'Alcohols',
-                    style: TextStyle(fontSize: 35),
+                    style: TextStyle(fontSize: 25),
                   ),
                 ),
               ),
             ],
           ),
+          ),
         ),
-        SizedBox(height: 10,),
-        Container(color: Colors.pink,
+        const SizedBox(height: 10,),
+        Flexible (
+          flex: 85,
+          child: Container(
+            color: Colors.white,
           child: const Center(
             child: Text(
               'Menus',
-              style: TextStyle(fontSize: 35),
+              style: TextStyle(fontSize: 25),
             ),
           ),
+        ),
         ),
       ],
     )
--- v1/lib/widgets/home.dart
+++ v1/lib/widgets/home.dart
@@ -3,26 +3,22 @@ import 'package:flutter/material.dart';
 import 'containers/menu.dart';
 import 'containers/orders.dart';
 
-// Container customMainContainer(){
-//   return Container(color: Color.fromARGB(255, 239, 240, 240),
-//     child: const Center(
-//       child: Text(
-//         'Home222',
-//         style: TextStyle(fontSize: 35),
-//       ),
-//     ),
-//   );
-// }
-
 Container customMainContainer(){
   return Container(color: Color.fromARGB(255, 239, 240, 240),
     child: Row(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisSize: MainAxisSize.max,
       children: [
-        manuContainer(),
+        Flexible (
+          flex: 60,
+          child: manuContainer(),
+        ),
         SizedBox(width: 10,),
-        ordersContainer(),
+        Flexible (
+          flex: 35,
+          child: ordersContainer(),
+        ),
       ],
     )
   );
