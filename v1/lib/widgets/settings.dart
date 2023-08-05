219560e6cf63aae687b8b8f10e4f72f4a32803e0
MinsukKim1806 <minsuk.kim.1806@gmail.com>
2023-08-05 14:10:26 +0200

    Add buttons for menus

--- v1/lib/widgets/settings.dart
+++ v1/lib/widgets/settings.dart
@@ -1,13 +1,88 @@
 import 'package:flutter/material.dart';
 
 Container customSettingsContainer(){
-  return Container(color: Colors.white,
-    child: const Center(
-      child: Text(
-        'Settingsss2222',
-        style: TextStyle(fontSize: 35),
-      ),
-    ),
+  return Container(color: Color.fromARGB(255, 239, 240, 240),
+    child: Column (
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Flexible (
+          flex: 40,
+          child: Container(
+            padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+            height: 40.0,
+            decoration: BoxDecoration(
+              color: Colors.lightBlue,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 4,
+                  offset: Offset(4, 8),
+                ),
+              ],
+            ),
+            child: const Center(
+              child: Text(
+                'Add Menu',
+                style: TextStyle(fontSize: 25),
+              ),
+            ),
+          ),
+        ),
+        SizedBox(height: 30,),
+        Flexible (
+          flex: 40,
+          child: Container(
+            padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+            height: 40.0,
+            decoration: BoxDecoration(
+              color: Colors.lightBlue,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 4,
+                  offset: Offset(4, 8),
+                ),
+              ],
+            ),
+            child: const Center(
+              child: Text(
+                'Update Menu',
+                style: TextStyle(fontSize: 25),
+              ),
+            ),
+          ),
+        ),
+        SizedBox(height: 30,),
+        Flexible (
+          flex: 40,
+          child: Container(
+            padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+            height: 40.0,
+            decoration: BoxDecoration(
+              color: Colors.lightBlue,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 4,
+                  offset: Offset(4, 8),
+                ),
+              ],
+            ),
+            child: const Center(
+              child: Text(
+                'Delete Menu',
+                style: TextStyle(fontSize: 25),
+              ),
+            ),
+          ),
+        ),
+      ],
+    )
   );
 }
   
